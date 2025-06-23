81e83a4addb27bd8bac91f9d701637407868c256
Patrick McDermott <mcd@omg.lol>
2025-06-23 16:45:22 +1200

    Function to save xml config.

--- src/xmlhandling.rs
+++ src/xmlhandling.rs
@@ -1,8 +1,10 @@
-use serde::Deserialize;
-use quick_xml::de::from_str;
+use serde::{Serialize, Deserialize,};
+use quick_xml::de::{from_str,};
+use quick_xml::se::{to_string,};
 use std::fs;
+use std::io::Write;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct ServerConfig {
     pub ip_address: String,
     pub port: u16,
@@ -13,4 +15,11 @@ pub fn load_config(path: &str) -> Result<ServerConfig, Box<dyn std::error::Error
     let xml = fs::read_to_string(path)?;
     let config: ServerConfig = from_str(&xml)?;
     Ok(config)
+}
+
+pub fn save_config(path: &str, config: &ServerConfig) -> Result<(), Box<dyn std::error::Error>> {
+    let xml = to_string(config)?;
+    let mut file = std::fs::File::create(path)?;
+    file.write_all(xml.as_bytes())?;
+    Ok(())
 }
